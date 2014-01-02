fb9a378b0a4d9a11241a6bd9dda3e3632630648b
Ania <aniaq@op.pl>
2014-01-03 00:45:41 +0100

    Pierwszy skrypt
    Skrypt do jeżdżenia autkiem ;)

---
+++ Assets/Resources/scripts/MoveCar.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCar : MonoBehaviour {
+
+    public float step = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float axis = Input.GetAxis("Horizontal");
+
+        Vector2 scale = transform.localScale;
+        Vector2 pos = transform.position;
+
+        if (axis < 0.0)
+        {
+            scale.x = -1.0f;
+            transform.localScale = scale;
+
+            pos.x += step * axis;
+            transform.position = pos;
+        }
+        else if (axis > 0.0)
+        {
+            scale.x = 1.0f;
+            transform.localScale = scale;
+
+            pos.x += step * axis;
+            transform.position = pos;
+        }
+
+	}
+}
