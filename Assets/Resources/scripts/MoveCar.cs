58b4f0a6100d9eee3aee293d50d430a081384d4f
Ania <aniaq@op.pl>
2014-01-03 11:12:12 +0100

    Skrypt do jeżdżenia
    Zmiana koncepcji ;) skrypt zamiast autka i kamery przesuwa tło.

--- Assets/Resources/scripts/MoveCar.cs
+++ Assets/Resources/scripts/MoveCar.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MoveCar : MonoBehaviour {
 
     public float step = 0.2f;
+	public List<GameObject> bg;
 
 	// Use this for initialization
 	void Start () {
@@ -14,25 +16,32 @@ public class MoveCar : MonoBehaviour {
 	void Update () {
         float axis = Input.GetAxis("Horizontal");
 
-        Vector2 scale = transform.localScale;
-        Vector2 pos = transform.position;
+		if (axis != 0.0) {
 
-        if (axis < 0.0)
-        {
-            scale.x = -1.0f;
-            transform.localScale = scale;
+			// przesuwanie tła
+			Vector2 pos;
+			foreach( GameObject bground in bg)
+			{
+				pos = bground.transform.position;
+				pos.x -= step * axis;
 
-            pos.x += step * axis;
-            transform.position = pos;
-        }
-        else if (axis > 0.0)
-        {
-            scale.x = 1.0f;
-            transform.localScale = scale;
+				if (pos.x < -37.0)
+					pos.x += 37.0f + (bg.Count - 1) * 29.65f;
 
-            pos.x += step * axis;
-            transform.position = pos;
-        }
+				bground.transform.position = pos;
+			}
+
+			// obrót auta
+			Vector2 scale = transform.localScale;
+			if (axis < 0.0) {
+					scale.x = -1.0f;
+					transform.localScale = scale;
+
+			} else if (axis > 0.0) {
+					scale.x = 1.0f;
+					transform.localScale = scale;
+			}
+		}
 
 	}
 }
