f003401c224b12149244fceabeb984d021fee39c
Ania <aniaq@op.pl>
2014-01-10 00:20:31 +0100

    przechodzenie do nast. levelu
    + teksturka dla otoczki

---
+++ Assets/Resources/scripts/LevelTrigger.cs
@@ -0,0 +1,26 @@
+﻿/// 
+/// Autor: Anna Kuśnierz
+/// Data: 8.01.2013
+/// 
+
+using UnityEngine;
+using System.Collections;
+
+public class LevelTrigger : MonoBehaviour {
+
+	public int nextLevel = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		Application.LoadLevel(nextLevel);
+	}
+}
